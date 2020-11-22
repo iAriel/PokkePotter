3a9f36d253b2c45e2f2a31bd625fc91e2bff82bf
matheusmaximianomv <matheusmaximianomv@aluno.fapce.edu.br>
2020-11-22 16:17:50 -0300

    Fix: Setando no estado global do tema a cor neutra

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,17 +4,22 @@ import './style.css';
 
 import api from '../../services/api';
 
+import { UseTheme } from '../../context/Theme';
+
 export default function Home() {
 
     const [houses, setHouses] = useState([]);
+    const { setTheme } = UseTheme();
+
     useEffect(() => { 
         async function getHouses(){
             const response = await api.get('/houses');
             setHouses(response.data);
         }
 
+        setTheme({ background: '#008ce0' });
         getHouses();
-     }, []); 
+     }, [setTheme]); 
 
     return (
         <main>
@@ -24,19 +29,19 @@ export default function Home() {
             </div>
             <div className="choice">
 
-                <Link to={`/characters/${houses[0]}`} className= "grifi">
+                <Link to={`/characters/${houses[0]}`} className= "grifi" onClick={() => setTheme({  background: '#97100a' })}>
                     <img src="
                     https://img.r7.com/images/casas-de-hogwarts-significados-por-tras-dos-lares-da-escola-bruxa-24082020072953571   " alt={houses[0]}/>
                 </Link>
 
-                <Link to={`/characters/${houses[1]}`} className= "sons">
+                <Link to={`/characters/${houses[1]}`} className= "sons" onClick={() => setTheme({  background: '#0b6242' })}>
                     <img src="https://img.r7.com/images/casas-de-hogwarts-significados-por-tras-dos-lares-da-escola-bruxa-24082020072953899" alt={houses[1]}/>
                 </Link>
 
-                <Link to={`/characters/${houses[2]}`} className= "corv">
+                <Link to={`/characters/${houses[2]}`} className= "corv" onClick={() => setTheme({  background: '#43547f' })}>
                     <img src="https://img.r7.com/images/casas-de-hogwarts-significados-por-tras-dos-lares-da-escola-bruxa-24082020072954238" alt={houses[2]}/>
                 </Link>
-                <Link to={`/characters/${houses[3]}`} className= "lufa">
+                <Link to={`/characters/${houses[3]}`} className= "lufa" onClick={() => setTheme({  background: '#ca9923' })}>
                     <img src="https://img.r7.com/images/casas-de-hogwarts-significados-por-tras-dos-lares-da-escola-bruxa-24082020072954574?dimensions=600x315&crop_position=c" alt={houses[3]}/>
                 </Link>
             </div>           
