db31d271e10aedec6022f843a02388bdd43727a2
matheusmaximianomv <matheusmaximianomv@aluno.fapce.edu.br>
2020-11-22 16:17:05 -0300

    Fix: Adicionando o componente ThemeProvider para que seus filhos possa acessar o estado global

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import {Route, BrowserRouter, Switch} from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
-import Characters  from './pages/Characters';
+import Characters from './pages/Characters';
 import WizardDetails from './pages/WizardDetails';
 
+import ThemeProvider from './context/Theme';
+
 export default function Routes() {
-    return(
+    return (
         <BrowserRouter>
-            <Nav />
-            <Switch>
-                <Route path="/" component={Home} exact/>
-                <Route path="/characters/:house" component={Characters}/>
-                <Route path="/wizards/:name" component = {WizardDetails}/>
-            </Switch>
-            <Footer />
+            <ThemeProvider>
+                <Nav />
+                <Switch>
+                    <Route path="/" component={Home} exact />
+                    <Route path="/characters/:house" component={Characters} />
+                    <Route path="/wizards/:name" component={WizardDetails} />
+                </Switch>
+                <Footer />
+            </ThemeProvider>
         </BrowserRouter>
     );
 }
