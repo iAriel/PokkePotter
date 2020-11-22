9cd694e4ec8f675ee924c1ad55809f10cea2268f
matheusmaximianomv <matheusmaximianomv@aluno.fapce.edu.br>
2020-11-22 16:19:31 -0300

    Fix: Alteração para usar o estado global e remoção de valores de estilo fixos

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { UseTheme } from '../../context/Theme';
+
 import './styles.css';
 
 export default function nav(){
+    const { theme } = UseTheme();
+
     return(
-        <nav>
+        <nav style={theme}>
             <div className="nav-wrapper">
                 <Link to="/" className="brand-logo center">PokePotter</Link>
             </div>
