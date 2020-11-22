fe5bb1e137853b5517bc3358a95a8e0ae1661cf1
matheusmaximianomv <matheusmaximianomv@aluno.fapce.edu.br>
2020-11-22 15:02:23 -0300

    Fix: Como a navbar e o footer é o mesmo em todas as páginas podemos  chamar eles aqui e só variar o conteúdo com as rotas

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Route, BrowserRouter, Switch} from 'react-router-dom';
 
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+
 import Home from './pages/Home';
 import Characters  from './pages/Characters';
 import WizardDetails from './pages/WizardDetails';
@@ -8,13 +11,13 @@ import WizardDetails from './pages/WizardDetails';
 export default function Routes() {
     return(
         <BrowserRouter>
+            <Nav />
             <Switch>
-
                 <Route path="/" component={Home} exact/>
                 <Route path="/characters/:house" component={Characters}/>
                 <Route path="/wizards/:name" component = {WizardDetails}/>
-                
             </Switch>
+            <Footer />
         </BrowserRouter>
     );
 }
