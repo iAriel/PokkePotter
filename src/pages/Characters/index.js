c1ceaa2e7360a8b4c2cee22c63e2674ebe3f98ca
matheusmaximianomv <matheusmaximianomv@aluno.fapce.edu.br>
2020-11-22 15:03:15 -0300

    FIx: Removendo os componentes Nav e Footer Usando a tag main no lugar do fragment

--- src/pages/Characters/index.js
+++ src/pages/Characters/index.js
@@ -1,25 +1,22 @@
-import React, {useEffect, useState} from 'react';
-import {useParams, Link} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
 
 import './styles.css';
 import api from '../../api';
 
-import Nav from '../../components/nav';
-import Footer from '../../components/footer';
+export default function Characters() {
+    const { house } = useParams();
 
-export default function Characters(){
-    const {house} = useParams();
-    
     const [wizards, setWizards] = useState([]);
 
     useEffect(() => {
-        async function getWizards(){
+        async function getWizards() {
             const response = await api.get(`/wizards/houses/${house}`);
             setWizards(response.data);
         }
 
         getWizards();
-    },[house]);
+    }, [house]);
 
     // const styles = (cor) =>{
     //     if(cor === "Gryffindor"){
@@ -31,37 +28,34 @@ export default function Characters(){
     //     }else if (cor ==="Hufflepuff"){
     //         return "#ca9923";
     //     }
-        
+
     // };
-    
-    return(
-        <div>
-            <Nav/>
+
+    return (
+        <main>
             <div className="head">
-                <img src="https://i.pinimg.com/originals/7a/37/c7/7a37c7b60c016089a64ec5effc1a2512.png" alt="Escudo de Hogwarts"/>
+                <img src="https://i.pinimg.com/originals/7a/37/c7/7a37c7b60c016089a64ec5effc1a2512.png" alt="Escudo de Hogwarts" />
             </div>
-            
+
             <div className="container">
                 <div className="row" >
                     {wizards.map(wizard => (
-                                    <div className="col s12 m3" key={wizard.name}>
-                                        <div className="card" id="carta">
-                                            <div className="card-image">
-                                                <Link to={`/wizards/${encodeURIComponent(wizard.name)}`}> 
-                                                    <img className="image" src={wizard.image} alt={wizard.name} />  
-                                                </Link>
-                                                
-                                                    <span className="card-title">
-                                                        <Link to={`/wizards/${encodeURIComponent(wizard.name)}`}> {wizard.name} </Link>
-                                                    </span>
-                                            </div>
-                                        </div>
-                                    </div>
+                        <div className="col s12 m3" key={wizard.name}>
+                            <div className="card" id="carta">
+                                <div className="card-image">
+                                    <Link to={`/wizards/${encodeURIComponent(wizard.name)}`}>
+                                        <img className="image" src={wizard.image} alt={wizard.name} />
+                                    </Link>
+
+                                    <span className="card-title">
+                                        <Link to={`/wizards/${encodeURIComponent(wizard.name)}`}> {wizard.name} </Link>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
                     ))}
-                </div>    
+                </div>
             </div>
-            
-                <Footer /> 
-        </div>
+        </main>
     );
 }
